1bc15041c69abb4a757580556d5df8ada591770d
Mojesh Ragireddy <mojesh@Mojeshs-MacBook-Pro.local>
2024-12-14 15:52:26 +0530

    add java directory to demo-app

---
+++ java/If.java
@@ -0,0 +1,28 @@
+class If{
+
+public static void main(String[]args){
+
+    for(int i = 1; i<=5; i++){
+System.out.println("Day" + i);
+for (int j=1; j<=i; j++){
+
+    System.out.println(j+8 + "pm" +" - " + (j+9) + "pm");
+}
+
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
